e5f464d65f3db350469afa581d97b445edbd4986
R. Fariz <farizramazanov38@gmail.com>
2022-11-12 13:33:04 +0400

    fix: change structure

--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import products from '../components/productsList/productsListSlice';
-
-export const store = configureStore({
-  reducer: { products },
-  devTools: process.env.NODE_ENV !== 'production'
-});
